da6530d8c664b704315ee862f93f9b82725091e8
shariful734 <shariful.sharifulrony@gmail>
2021-11-13 23:05:44 +0600

    done

--- index.js
+++ index.js
@@ -23,13 +23,16 @@ async function run() {
         await client.connect();
         const database = client.db("BuzzxCycles");
         const productsCollection = database.collection("products")
+        const ordersCollection = database.collection("orders")
+        const reviewsCollection = database.collection("reviews")
+        const usersCollection = database.collection("users")
 
         // getting api 
         app.get('/products', async (req, res) => {
             const cursor = productsCollection.find({});
             const products = await cursor.toArray();
             res.send(products);
-        })
+        });
 
         // posting data 
 
@@ -37,9 +40,29 @@ async function run() {
             const product = req.body;
             console.log('hit the post api');
             const result = await productsCollection.insertOne(product);
+            // console.log(result);
+            res.json(result);
+        });
+
+        // posting orders api 
+
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            console.log('hit the post api');
+            const result = await ordersCollection.insertOne(order);
             console.log(result);
             res.json(result);
-        })
+        });
+
+        // getting order api 
+
+        app.get('/orders', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const cursor = ordersCollection.find({});
+            const orders = await cursor.toArray();
+            res.send(orders);
+        });
 
         // specific api with id 
 
@@ -49,8 +72,74 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const order = await productsCollection.findOne(query);
             res.json(order);
-        })
+        });
 
+        // posting review api 
+
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            console.log('hit the post api');
+            const result = await reviewsCollection.insertOne(review);
+            console.log(result);
+            res.json(result);
+        });
+
+        // getting reviews api 
+
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewsCollection.find({});
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
+        // posting users 
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log('hit the post api');
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        });
+
+
+
+
+
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const filter = { email: user.email };
+            const updatedDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updatedDoc);
+            res.json(result);
+        });
+
+        app.get('/users', async (req, res) => {
+            const cursor = usersCollection.find({});
+            const users = await cursor.toArray();
+            console.log(users);
+            res.send(users);
+        });
+
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        });
+
+        // getting orders api 
+
+        app.get('/orders', async (req, res) => {
+            const cursor = ordersCollection.find({});
+            const orders = await cursor.toArray();
+            res.send(orders);
+        });
 
     }
     finally {
